11eafb60c92a90bca80238509f123ac581487264
akakshuki <giangbaccai1207@gmail.com>
2020-02-10 14:29:38 +0700

    set up database

--- api/Startup.cs
+++ api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,7 +29,7 @@ namespace api
         {
             services.AddControllers();
 
-            services.AddDbContext<APIDbContext>();
+            services.AddDbContext<APIDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
