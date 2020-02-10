6c3303277d132cd5d63f1e7e4a5639bb7f31ecd7
akakshuki <giangbaccai1207@gmail.com>
2020-02-10 14:00:14 +0700

    first commit

---
+++ api/EF/APIDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models.EF
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+    }
+}
